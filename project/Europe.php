8ac20b37bd027ab1a570d2395e7227e042015ed0
Edgar <mmnledgar@gmail.com>
2020-07-13 16:03:39 +0000

    realisation de la page pour europe

--- project/Europe.php
+++ project/Europe.php
@@ -15,21 +15,21 @@ $title = "EUROPE | NEWS "
     <ul>
     <li>  </li>
        <li> <a id="bg" class="api" href="">Bulgarie </a> </li>
-       <li> <a id="be" href="">Belgique </a> </li>
-        <li><a id="cz" href="">Republique Tcheque</a></li>
-        <li><a id="fr" href="">France</a></li>
-        <li><a id="hu" href="">Hongrie</a></li>
-        <li><a id="de" href="">Allemagne</a></li>
-        <li><a id="it" href="">Italie</a></li>
-        <li><a id="ca" href="">Canada</a></li>
-        <li><a id="gb" href="">Royaume-Uni</a></li>
-        <li><a id="gr" href="">Grece</a></li>
-        <li><a id="ie" href="">Irlande</a></li>
-        <li><a id="ca" href="">Canada</a></li>
-        <li><a id="nl" href="">Pays-Bas</a></li>
-        <li><a id="pl" href="">Pologne</a></li>
-        <li><a id="pt" href="">Portugal</a></li>
-        <li><a id="tr" href="">Turquie</a></li>
+       <li> <a id="be" class="api" href="">Belgique </a> </li>
+        <li><a id="cz" class="api" href="">Republique Tcheque</a></li>
+        <li><a id="fr" class="api" href="">France</a></li>
+        <li><a id="hu" class="api" href="">Hongrie</a></li>
+        <li><a id="de" class="api" href="">Allemagne</a></li>
+        <li><a id="it" class="api" href="">Italie</a></li>
+        <li><a id="ca" class="api" href="">Canada</a></li>
+        <li><a id="gb" class="api" href="">Royaume-Uni</a></li>
+        <li><a id="gr" class="api" href="">Grece</a></li>
+        <li><a id="ie" class="api" href="">Irlande</a></li>
+        <li><a id="ca" class="api" href="">Canada</a></li>
+        <li><a id="nl" class="api" href="">Pays-Bas</a></li>
+        <li><a id="pl" class="api" href="">Pologne</a></li>
+        <li><a id="pt" class="api" href="">Portugal</a></li>
+        <li><a id="tr" class="api" href="">Turquie</a></li>
     </ul>
 </div>
 
